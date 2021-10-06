bc790240b830ead6773c99c1b84f5725a5505472
Panadestein <rpana92@gmail.com>
2021-10-06 02:32:41 +0200

    Fixed poor config's options

---
+++ .emacs.d/elpa/raku-mode-20210412.2342/raku-mode-pkg.el
@@ -0,0 +1,13 @@
+(define-package "raku-mode" "20210412.2342" "Major mode for editing Raku code"
+  '((emacs "24.4")
+    (pkg-info "0.1"))
+  :commit "7496ad3a03bed613c259405ec8839ae02950fdb1" :authors
+  '(("Hinrik Örn Sigurðsson" . "hinrik.sig@gmail.com"))
+  :maintainer
+  '("Hinrik Örn Sigurðsson" . "hinrik.sig@gmail.com")
+  :keywords
+  '("languages")
+  :url "https://github.com/hinrik/perl6-mode")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
